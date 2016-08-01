6add5723972c818ded99fe38de536d4a20471435
U-METNET\tgronka <tgronka@US1277496.metnet.net>
2016-08-01 09:32:20 -0400

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+Simple installer script for the azure storage account docker plugin.
+
+Works for Docker v1.11.
---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo "Checking for root priviledges.."
+ROOT_UID=0
+if [ $UID != $ROOT_UID ]; then
+	echo "This script must be ran with sudo."
+	exit 1
+fi
+echo "Check passed."
+
+echo "This script will install the Azure docker volume driver 0.4.0"
+echo "Driver 0.4.0 might be incompatible with docker versions beyond 1.11"
+
+wget -qO /usr/bin/azurefile-dockervolumedriver https://github.com/Azure/azurefile-dockervolumedriver/releases/download/0.4.0/azurefile-dockervolumedriver
+chmod +x /usr/bin/azurefile-dockervolumedriver
+
+cp azurefile-dockervolumedriver.default /etc/default/azurefile-dockervolumedriver
+cp azurefile-dockervolumedriver.service /etc/systemd/azurefile-dockervolumedriver.service
+
+echo "username=" >> /etc/default/azurefile-dockervolumedriver
+echo "password=" >> /etc/default/azurefile-dockervolumedriver
+
+echo "Storage account credentials are set in /etc/default/azurefile-dockervolumedriver."
+echo "You will have to add your username and password."
+
+mkdir -p /etc/systemd/systemd
+systemctl daemon-reload
+systemctl enable azurefile-dockervolumedriver
+systemctl start azurefile-dockervolumedriver
+
+echo "azurefile-dockervolumedriver status:"
+systemctl status azurefile-dockervolumedriver
+
