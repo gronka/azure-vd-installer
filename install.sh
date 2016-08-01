8c27f37f1bd47ad61185e0a6521d31137145b42c
U-METNET\tgronka <tgronka@US1277496.metnet.net>
2016-08-01 10:51:18 -0400

    interactive username/pass input added

--- install.sh
+++ install.sh
@@ -9,20 +9,30 @@ echo "Check passed."
 
 echo "This script will install the Azure docker volume driver 0.4.0"
 echo "Driver 0.4.0 might be incompatible with docker versions beyond 1.11"
+echo ""
 
 wget -qO /usr/bin/azurefile-dockervolumedriver https://github.com/Azure/azurefile-dockervolumedriver/releases/download/0.4.0/azurefile-dockervolumedriver
 chmod +x /usr/bin/azurefile-dockervolumedriver
 
 cp azurefile-dockervolumedriver.default /etc/default/azurefile-dockervolumedriver
-cp azurefile-dockervolumedriver.service /etc/systemd/azurefile-dockervolumedriver.service
+cp azurefile-dockervolumedriver.service /lib/systemd/system/azurefile-dockervolumedriver.service
 
 echo "Storage account credentials are set in /etc/default/azurefile-dockervolumedriver."
-echo "You will have to add your username and password."
+echo "We will set your credentials now."
+
+echo "Name: AZURE_STORAGE_ACCOUNT="
+read AZURE_STORAGE_ACCOUNT
+echo "AZURE_STORAGE_ACCOUNT="$AZURE_STORAGE_ACCOUNT >> /etc/default/azurefile-dockervolumedriver
+echo "Pass: AZURE_STORAGE_ACCOUNT_KEY="
+read AZURE_STORAGE_ACCOUNT_KEY
+echo "AZURE_STORAGE_ACCOUNT_KEY="$AZURE_STORAGE_ACCOUNT_KEY >> /etc/default/azurefile-dockervolumedriver
 
 mkdir -p /etc/systemd/systemd
 systemctl daemon-reload
 systemctl enable azurefile-dockervolumedriver
 systemctl start azurefile-dockervolumedriver
+
+echo ""
 
 echo "azurefile-dockervolumedriver status:"
 systemctl status azurefile-dockervolumedriver
